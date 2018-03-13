f0e10bdb3a2a7056b22689e30509261ebfa8dde1
Vladislav <vlad_24_9898@mail.ru>
2018-03-13 22:53:10 +0300

    first commit

---
+++ Trx/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace Trx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbWebCamPreview = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lbCams = new System.Windows.Forms.ListBox();
+            this.decTraine = new System.Windows.Forms.Button();
+            this.gbUserData = new System.Windows.Forms.GroupBox();
+            this.cbTraine = new System.Windows.Forms.ComboBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWebCamPreview)).BeginInit();
+            this.gbUserData.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbWebCamPreview
+            // 
+            this.pbWebCamPreview.Location = new System.Drawing.Point(3, 3);
+            this.pbWebCamPreview.Name = "pbWebCamPreview";
+            this.pbWebCamPreview.Size = new System.Drawing.Size(354, 273);
+            this.pbWebCamPreview.TabIndex = 0;
+            this.pbWebCamPreview.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(654, 6);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(90, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = " Сканировать";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lbCams
+            // 
+            this.lbCams.FormattingEnabled = true;
+            this.lbCams.Location = new System.Drawing.Point(363, 6);
+            this.lbCams.Name = "lbCams";
+            this.lbCams.Size = new System.Drawing.Size(143, 30);
+            this.lbCams.TabIndex = 2;
+            // 
+            // decTraine
+            // 
+            this.decTraine.Location = new System.Drawing.Point(512, 6);
+            this.decTraine.Name = "decTraine";
+            this.decTraine.Size = new System.Drawing.Size(119, 23);
+            this.decTraine.TabIndex = 4;
+            this.decTraine.Text = "Считать тренировку";
+            this.decTraine.UseVisualStyleBackColor = true;
+            this.decTraine.Click += new System.EventHandler(this.decTraine_Click);
+            // 
+            // gbUserData
+            // 
+            this.gbUserData.Controls.Add(this.cbTraine);
+            this.gbUserData.Controls.Add(this.textBox4);
+            this.gbUserData.Controls.Add(this.textBox5);
+            this.gbUserData.Controls.Add(this.label6);
+            this.gbUserData.Controls.Add(this.textBox3);
+            this.gbUserData.Controls.Add(this.textBox2);
+            this.gbUserData.Controls.Add(this.textBox1);
+            this.gbUserData.Controls.Add(this.label5);
+            this.gbUserData.Controls.Add(this.label4);
+            this.gbUserData.Controls.Add(this.label3);
+            this.gbUserData.Controls.Add(this.label2);
+            this.gbUserData.Controls.Add(this.label1);
+            this.gbUserData.Location = new System.Drawing.Point(363, 49);
+            this.gbUserData.Name = "gbUserData";
+            this.gbUserData.Size = new System.Drawing.Size(381, 171);
+            this.gbUserData.TabIndex = 5;
+            this.gbUserData.TabStop = false;
+            this.gbUserData.Text = "Данные";
+            // 
+            // cbTraine
+            // 
+            this.cbTraine.FormattingEnabled = true;
+            this.cbTraine.Location = new System.Drawing.Point(139, 67);
+            this.cbTraine.Name = "cbTraine";
+            this.cbTraine.Size = new System.Drawing.Size(100, 21);
+            this.cbTraine.TabIndex = 14;
+            this.cbTraine.SelectedIndexChanged += new System.EventHandler(this.cbTraine_SelectedIndexChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(139, 105);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(139, 88);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Дата приобретения";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(139, 47);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(139, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Количество занятий";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Купленный пакет";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Отчество";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Имя";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Фамилия";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Location = new System.Drawing.Point(2, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(761, 308);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.gbUserData);
+            this.tabPage1.Controls.Add(this.pbWebCamPreview);
+            this.tabPage1.Controls.Add(this.lbCams);
+            this.tabPage1.Controls.Add(this.decTraine);
+            this.tabPage1.Controls.Add(this.btnStart);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(753, 282);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Главная";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(753, 282);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Пользователь";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(753, 282);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Статистика";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(753, 282);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Расписание";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(753, 282);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Сотрудника";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 332);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pbWebCamPreview)).EndInit();
+            this.gbUserData.ResumeLayout(false);
+            this.gbUserData.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbWebCamPreview;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lbCams;
+        private System.Windows.Forms.Button decTraine;
+        private System.Windows.Forms.GroupBox gbUserData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.ComboBox cbTraine;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ Trx/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Data.SqlClient;
+
+namespace Trx
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlConnection;
+        string userId;
+        string stringConnection;
+
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        private ZXing.BarcodeReader reader;
+
+        delegate void SetStringDelegate(String parameter);
+        delegate void SetTextBoxDelegate(TextBox textBox, String parameter);
+        delegate void SetComboBoxDelegate(ComboBox comboBox, String parameter);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void SetResult(string result)
+        {
+            if(!InvokeRequired)
+                userId = result;
+            else
+                Invoke(new SetStringDelegate(SetResult), new object[] { result });
+        }
+
+        void SetTextBox(TextBox textBox, string result)
+        {
+            if (!InvokeRequired)
+                textBox.Text = result;
+            else
+                Invoke(new SetTextBoxDelegate(SetTextBox), new object[] { textBox, result });
+        }
+        
+        private void SetComboBox(ComboBox comboBox, string v)
+        {
+            if (!InvokeRequired)
+            {
+                comboBox.Items.Add(v);
+                comboBox.SelectedText = "---";
+            }
+            else
+                Invoke(new SetComboBoxDelegate(SetComboBox), new object[] { comboBox, v });
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            stringConnection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Vladislav\source\repos\Trx\Trx\Database.mdf;Integrated Security=True";
+
+            sqlConnection = new SqlConnection(stringConnection);
+
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            reader = new ZXing.BarcodeReader();
+            reader.Options.PossibleFormats = new List<ZXing.BarcodeFormat>();
+            reader.Options.PossibleFormats.Add(ZXing.BarcodeFormat.QR_CODE);
+
+            if(videoDevices.Count > 0)
+            {
+                foreach(FilterInfo device in videoDevices)
+                {
+                    lbCams.Items.Add(device.Name);
+                }
+                lbCams.SelectedIndex = 0;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            videoSource = new VideoCaptureDevice(videoDevices[lbCams.SelectedIndex].MonikerString);
+            videoSource.NewFrame += new NewFrameEventHandler(video_newFrame);
+            videoSource.Start();
+        }
+
+        private void video_newFrame(object sender, NewFrameEventArgs e)
+        {
+            Bitmap bitmap = (Bitmap)e.Frame.Clone();
+
+            pbWebCamPreview.Image = bitmap;
+
+            ZXing.Result result = reader.Decode((Bitmap)e.Frame.Clone());
+            if(result != null)
+            {
+                MessageBox.Show(
+                    "Пользователь найден",
+                    "Пользователь найден",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information,
+                    MessageBoxDefaultButton.Button1,
+                    MessageBoxOptions.DefaultDesktopOnly);
+                SetResult(result.Text);
+
+                sqlConnection = new SqlConnection(stringConnection);
+                try
+                {
+                    sqlConnection.Open();
+                }
+                catch (Exception ex)
+                {
+
+                }
+                SqlCommand sqlCommand = new SqlCommand("SELECT * FROM [User] WHERE Id = " + userId, sqlConnection);
+                using (SqlDataReader dr = sqlCommand.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    while (dr.Read())
+                    {
+                        SetTextBox(textBox1, dr.GetValue(1).ToString().Trim());
+                        SetTextBox(textBox2, dr.GetValue(2).ToString().Trim());
+                        SetTextBox(textBox3, dr.GetValue(3).ToString().Trim());
+                    }
+                }
+                sqlConnection.Close();
+                sqlConnection.Dispose();
+
+                sqlConnection = new SqlConnection(stringConnection);
+                try
+                {
+                    sqlConnection.Open();
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+                SqlCommand sqlCommand1 = new SqlCommand("SELECT * FROM [UserTraine] WHERE id_user = " + userId, sqlConnection);
+                using (SqlDataReader dr = sqlCommand1.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    while (dr.Read())
+                    {
+                        SetComboBox(cbTraine, dr.GetValue(2).ToString().Trim());
+                    }
+                }
+                sqlConnection.Close();
+                sqlConnection.Dispose();
+
+                if (videoSource != null)
+                {
+                    videoSource.SignalToStop();
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(videoSource != null)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+            }
+        }
+
+        private void cbTraine_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedState = cbTraine.SelectedItem.ToString();
+            sqlConnection = new SqlConnection(stringConnection);
+            try
+            {
+                sqlConnection.Open();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            SqlCommand sqlCommand = new SqlCommand("SELECT * FROM [UserTraine] WHERE id_user = " + userId + " AND traine_type = N'" + selectedState + "'", sqlConnection);
+            using (SqlDataReader dr = sqlCommand.ExecuteReader(CommandBehavior.CloseConnection))
+            {
+                while (dr.Read())
+                {
+                    SetTextBox(textBox5, dr.GetValue(4).ToString().Trim());
+                    SetTextBox(textBox4, dr.GetValue(5).ToString().Trim());
+                }
+            }
+            sqlConnection.Close();
+            sqlConnection.Dispose();
+        }
+
+        private void decTraine_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Вы уверены, что хотите списать тренировку?",
+                "Списание 1-ой тренировки",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button2,
+                MessageBoxOptions.DefaultDesktopOnly);
+            if(result == DialogResult.Yes)
+            {
+                sqlConnection = new SqlConnection(stringConnection);
+                try
+                {
+                    sqlConnection.Open();
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+                int dec = Convert.ToInt32(textBox5.Text) - 1;
+                SqlCommand sqlCommand = new SqlCommand("UPDATE [UserTraine] SET count_traine = " + dec + " WHERE id_user = " + userId + " AND traine_type = N'" + cbTraine.SelectedItem.ToString() + "'", sqlConnection);
+                int rowCount = sqlCommand.ExecuteNonQuery();
+                textBox5.Text = dec.ToString();
+                sqlConnection.Close();
+                sqlConnection.Dispose();
+            }
+        }
+    }
+}
